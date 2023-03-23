47d3355962705fa3f528bf362e74db8c31659626
yashpanwala34 <128489113+yashpanwala34@users.noreply.github.com>
2023-03-23 08:25:31 -0400

    Add files via upload

---
+++ main (1).c
@@ -0,0 +1,21 @@
+#include <kipr/wombat.h>
+
+int main()
+{
+    create_connect();
+    set_create_distance(0);
+    while(get_create_distance()<1000)
+    {
+    create_drive_direct(250,250);
+        msleep(10);
+    }
+// drive forward to first (red) pom in claws with servo arm in right position
+// turn right 90 degrees and move servo arm to left position to secure first pom
+// drive forward for 
+// after msleep(1000) pick up second (green) pom and move servo arm to right position to secure second pom
+// after msleep(1000) pick up third (red) pom and move servo arm to left position to secure third pom
+//
+    create_stop();
+    create_disconnect();
+    return 0;
+}
